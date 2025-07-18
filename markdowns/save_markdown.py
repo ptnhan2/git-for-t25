ee27920c25e3a7220f8938b4d357d0e7eaa80737
unknown <phanthiennhan248@gmail.com>
2025-07-19 00:04:09 +0700

    feat: add logic to save cleaned markdown to markdowns/md_output

---
+++ markdowns/save_markdown.py
@@ -0,0 +1,38 @@
+import os
+from slugify import slugify
+import shutil 
+from scraper.fetch_articles import get_all_articles, get_article_detail
+from markdowns.html_to_md import clean_and_convert_html
+
+OUTPUT_DIR = os.path.join("markdowns", "md_output")
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+def create_markdowns_directory():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+        print(f"Created directory: {OUTPUT_DIR}")
+    else: 
+        print(f"Directory already exists: {OUTPUT_DIR}")
+    articles = get_all_articles(max_articles=40)
+    for i, article in enumerate(articles):
+        article_id = article["id"]
+        title, html_body = get_article_detail(article_id)
+
+        markdown = clean_and_convert_html(html_body)
+        slug = slugify(title)
+
+        file_path = os.path.join(OUTPUT_DIR, f"{slug}.md")
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(f"# {title}\n\n")
+            f.write(markdown)
+
+        print(f"[{i+1}] Saved: {file_path}")
+
+
+def delete_markdowns_directory():
+	if os.path.exists(OUTPUT_DIR):
+		shutil.rmtree(OUTPUT_DIR)
+		print(f"Deleted directory: {OUTPUT_DIR}")
+	else:
+		print(f"Directory does not exist: {OUTPUT_DIR}")
+          
