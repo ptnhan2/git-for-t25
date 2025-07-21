5fa318747aacd898c27750fef50d7dad147546c7
unknown <phanthiennhan248@gmail.com>
2025-07-21 13:38:12 +0700

    feat: add chunking method

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from markdowns.save_markdown import create_markdowns_directory
 from markdowns.save_markdown import delete_markdowns_directory
-
+from markdowns.chunking.save_chunks_to_md import save_chunks_markdowns
 def main():
-	create_markdowns_directory()
+	# create_markdowns_directory()
 	# delete_markdowns_directory()
 
+	save_chunks_markdowns()
 
 if __name__ == "__main__":
     main()
