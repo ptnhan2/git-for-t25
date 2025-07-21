2c8253a6267eb30623127be0caa75284727c83cd
unknown <phanthiennhan248@gmail.com>
2025-07-21 13:53:27 +0700

    fix: fix syntax

--- markdowns/chunking/save_chunks_to_md.py
+++ markdowns/chunking/save_chunks_to_md.py
@@ -7,15 +7,14 @@ def convert_chunk_to_md(chunk):
     for i in range(1,6):
         key = f"Header {i}"
         if key in chunk.metadata:
-            md += f"{'#' * i} {chunk.metadata[key]}\n\n"    
+            md += f"'#' * {i} {chunk.metadata[key]}\n\n"    
     md += "\n" + chunk.page_content
     return md
     
 
+
 def save_chunks_markdowns(): 
-    print("Saving chunks to markdown...")
     chunks = mds_to_chunks()
-    print(chunks)
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
         print(f"Created directory: {OUTPUT_DIR}")
@@ -25,5 +24,5 @@ def save_chunks_markdowns():
     with open(file_path, "w", encoding="utf-8") as f:
         for chunk in chunks:
             md = convert_chunk_to_md(chunk)
-            f.write(md + "\n\n ------- \n\n")        
+            f.write(md + "\n\n --- \n\n")        
     print(f"Saved: {file_path}")
